a7df80a02737b4b1bf18bc0faf459a8289995bd0
wolfets <wolfets@free.fr>
2019-04-15 16:46:58 +0200

    Initial commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,149 @@
+using DBFirstDevelopment.Models.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBFirstDevelopment.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly EFCoreDBFirstDemoContext _context;
+
+        public StudentsController(EFCoreDBFirstDemoContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Student.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .SingleOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StudentId,FirstName,LastName,Gender,DateOfBirth,DateOfRegistration,PhoneNumber,Email,Address1,Address2,City,State,Zip")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.SingleOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("StudentId,FirstName,LastName,Gender,DateOfBirth,DateOfRegistration,PhoneNumber,Email,Address1,Address2,City,State,Zip")] Student student)
+        {
+            if (id != student.StudentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.StudentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .SingleOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var student = await _context.Student.SingleOrDefaultAsync(m => m.StudentId == id);
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool StudentExists(long id)
+        {
+            return _context.Student.Any(e => e.StudentId == id);
+        }
+    }
+}
---
+++ Models/DB/EFCoreDBFirstDemoContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DBFirstDevelopment.Models.DB
+{
+    public partial class EFCoreDBFirstDemoContext : DbContext
+    {
+        public virtual DbSet<Student> Student { get; set; }
+
+        public EFCoreDBFirstDemoContext(DbContextOptions<EFCoreDBFirstDemoContext> options)
+: base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Address1).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Address2).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.City).HasColumnType("varchar(30)");
+
+                entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+
+                entity.Property(e => e.DateOfRegistration).HasColumnType("datetime");
+
+                entity.Property(e => e.Email).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.FirstName).HasColumnType("varchar(30)");
+
+                entity.Property(e => e.Gender).HasColumnType("varchar(10)");
+
+                entity.Property(e => e.LastName).HasColumnType("varchar(30)");
+
+                entity.Property(e => e.PhoneNumber).HasColumnType("varchar(20)");
+
+                entity.Property(e => e.State).HasColumnType("varchar(30)");
+
+                entity.Property(e => e.Zip).HasColumnType("nchar(10)");
+            });
+        }
+    }
+}
